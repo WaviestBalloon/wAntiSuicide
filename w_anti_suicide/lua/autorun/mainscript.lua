44e667469c8bd05daf511a1a7244717f233a4888
Waviest <31796727+WaviestBalloon@users.noreply.github.com>
2020-11-06 00:17:22 +0000

    Update mainscript.lua
    Added 'PlaySound' you can now play sounds on the clientside
    Added new variables in order to toggle playing sounds on
    Added server-controlled convars although I might have not configured it correctly
    (was_toggle_antisuicide - Toggle anti-suicide
    was_toggle_playsound - Toggle sound played
    was_playsound_loc - Location of the sound played
    was_message - Message given to client)
    Cleaned my spaghetti up a bit
    Removed config variables, since you know, ConVar support now
    Removed unneeded crap from the script

--- w_anti_suicide/lua/autorun/mainscript.lua
+++ w_anti_suicide/lua/autorun/mainscript.lua
@@ -1,20 +1,22 @@
+print("[wAntiSuicide] Loading...")
 --                        _   _        _____       _      _     _      
 --            /\         | | (_)      / ____|     (_)    (_)   | |     
 -- __      __/  \   _ __ | |_ _ _____| (___  _   _ _  ___ _  __| | ___ 
 -- \ \ /\ / / /\ \ | '_ \| __| |______\___ \| | | | |/ __| |/ _` |/ _ \
 --  \ V  V / ____ \| | | | |_| |      ____) | |_| | | (__| | (_| |  __/
 --   \_/\_/_/    \_\_| |_|\__|_|     |_____/ \__,_|_|\___|_|\__,_|\___|
--- wAnti-Suicide, built by WaviestBalloon, made for my server, http://waviestnetworks.waviestballoon.uk but open for all.
--- Built with LOVE
+-- wAnti-Suicide, built by WaviestBalloon, made for my server (But you can use it)
+-- https://github.com/WaviestBalloon/wAntiSuicide
 
--- >>>>>>>>>  START OF CONFIG  <<<<<<<<<
-local SuicideMessage = "Stuck? Good luck getting out..."
-local CanSuicide = false
--- >>>>>>>>>   END OF CONFIG   <<<<<<<<<
 
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- WARNING! DON'T CHANGE ANYTHING BELOW, THIS WILL HELP YOU AND ME FIX ANY ISSUES YOU GET WITH THIS ADDON!
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--[[	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	WARNING! DON'T CHANGE ANYTHING BELOW, UNLESS YOU KNOW WHAT YOU'RE DOING
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!		]]
+
+local SuicideMessage = CreateConVar("was_message", "This server does not allow the kill command!", {FCVAR_SERVER_CAN_EXECUTE})
+local PlaySound = CreateConVar("was_toggle_playsound", false, {FCVAR_SERVER_CAN_EXECUTE})
+local Sound = CreateConVar("was_playsound_loc", "buttons/button2.wav", {FCVAR_SERVER_CAN_EXECUTE})
+local CanSuicide = CreateConVar("was_toggle_antisuicide", true, {FCVAR_SERVER_CAN_EXECUTE})
 
 if CanSuicide == true then
 	print("[wAnti-Suicide] 'CanSuicide' = true; this means players will be allowed to suicide")
@@ -26,24 +28,27 @@ else
 	end
 end
 
--- If you know what you're doing, feel free to change anything below, please do not go pass the 'hook.Add' part.
--- The reason I told you to not change anything up top is because it's a checker, to check if you've done the config correctly.
--- Anyways, if you do change anything below I will not give you help/support.
 local function BlockSuicide(ply)
 	if CanSuicide == false then
-		ply:ChatPrint(SuicideMessage)
 		print("[wAnti-Suicide] Player attempted a kill command; blocked due to 'CanSuicide' = false")
-		print("[wAnti-Suicide] SuicideMessage printed")
+		ply:ChatPrint(SuicideMessage)
+		print("[wAnti-Suicide] Suicide Message printed")
+		if PlaySound == true then
+			ply:surface.PlaySound(Sound)
+		end
 		return false
 	else
 		if CanSuicide == true then
-			print("[wAnti-Suicide] 'CanSuicide' = true; Suicide is allowed not blocking")
+			print("[wAnti-Suicide] 'CanSuicide' = true; Suicide is allowed, not blocking")
 		else
-			print("[wAnti-Suicide] Something isn't right; 'CanSuicide' doesn't equal to true or false")
+			print("[wAnti-Suicide] Error! Something isn't right; 'CanSuicide' doesn't equal to true or false")
 		end
 	end
 end
+
+-- HAH! NO COMMENTS EXPLAINING THE CODE! TAKE THAT COMPUTER SCIENCE COURSE!
+
 print("[wAnti-Suicide] Adding hook")
-hook.Add( "CanPlayerSuicide", "BlockSuicide", BlockSuicide )
+hook.Add("CanPlayerSuicide", "BlockSuicide", BlockSuicide)
 print("[wAnti-Suicide] Hook added")
 print("[wAnti-Suicide] Script loaded")
